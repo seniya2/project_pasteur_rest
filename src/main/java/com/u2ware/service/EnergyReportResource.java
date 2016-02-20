d1cc70c7010d3556dd51d4d37e0cadd5aeac4822
seniya2 <seniya2@u2ware.com>
2016-02-20 20:30:32 +0900

    .

--- src/main/java/com/u2ware/service/EnergyReportResource.java
+++ src/main/java/com/u2ware/service/EnergyReportResource.java
@@ -1,5 +1,6 @@
 package com.u2ware.service;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,11 +15,13 @@ import com.u2ware.domain.EnergyReport;
 @RepositoryRestResource(collectionResourceRel = "energyReport", path = "energyReport")
 public interface EnergyReportResource extends PagingAndSortingRepository<EnergyReport, Long>{
 	
-	Sort sort = new Sort(Direction.DESC, "daily");
-	Pageable pageable = new PageRequest(0, 20, sort);
+	Sort sort = new Sort(Direction.ASC, "daily");
+	Pageable pageable = new PageRequest(0, 50, sort);
 		
 	Iterable<EnergyReport> findAll(Sort sort);
 	Iterable<EnergyReport> findByDaily(@Param("daily") String Daily);
-	Iterable<EnergyReport> findByDailyContains(@Param("daily") String Daily);
+	//Iterable<EnergyReport> findByDailyContains(@Param("daily") String Daily);
+	
+	Page<EnergyReport> findByDailyContains(@Param("daily") String Daily, Pageable pageable);
 	
 }
